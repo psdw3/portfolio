9c536c67198dd1818dad0130ac06a504bcdf6f00
Tugy <daynjawmunkhtuguldur@gmail.com>
2024-03-29 17:21:23 +0800

    oh

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -11,21 +10,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://unpkg.com/aos@2.3.1/dist/aos.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   return (
     <html lang="en" data-theme="cupcake" >
-      <head>
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-      </head>
       <body className={inter.className}>
         <Header/>
         {children}
